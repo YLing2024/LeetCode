49bfaa95910274d906a1031d7e7bd5ff0952fc33
zhangyunling <1461428394@qq.com>
2025-04-22 17:13:48 +0800

    feat(dynamic-programming): 添加最大正方形和最长回文子串问题的解决方案
    - 新增最大正方形问题的解法，使用二维数组作为dp数组，实现状态转移方程
    - 添加最长回文子串问题的解决方案，采用动态规划方法

--- 动态规划基础版/12.最大正方形.ts
+++ 动态规划基础版/12.最大正方形.ts
@@ -29,3 +29,51 @@ matrix[i][j] 为 '0' 或 '1'
 
  */
 //战斗
+//本题需要一个二维数组作为dp数组，dp[i][j]表示以i,j为右下角的最大正方形尺寸。
+//本题目的难点是状态转移方程以及dp数组的含义问题。
+//对于状态转移方程，dp[i+1][j+1] = matrix[i+1][j+1] == 1 ? min(dp[i+1][j+1],dp[i][j+1],dp[i+1][j]) + 1 : 0;
+//这里直接给出状态转移方程，我会在自己博客里说一下详细的思考过程。
+//初始化也很简单，第一行和第一列可以直接初始化，遇到0就是0，遇到1就是1。
+
+function maximalSquare(matrix: string[][]): number {
+    const dp: Array<Array<number>> = [];
+    let maxSize = 0;
+    //行初始化
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === "1") {
+            dp[i] = [1];
+            maxSize = 1;
+        } else {
+            dp[i] = [0];
+        }
+    }
+    //列初始化
+    for (let j = 0; j < matrix[0].length; j++) {
+        if (matrix[0][j] === "1") {
+            dp[0][j] = 1;
+            maxSize = 1;
+        } else {
+            dp[0][j] = 0;
+        }
+    }
+    //第二行开始遍历
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][j] === "1") {
+                dp[i][j] =
+                    Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+                if (dp[i][j] > maxSize) maxSize = dp[i][j];
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    return maxSize ** 2;
+}
+maximalSquare([
+    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "1", "1"],
+    ["1", "1", "1", "1", "1"],
+    ["1", "0", "0", "1", "0"],
+]);
